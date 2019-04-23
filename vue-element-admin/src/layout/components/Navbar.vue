2288b2b69554d2dc7b4a46e6696eed8aabab181f
fairy·yue <gy99666@163.com>
2019-04-23 10:28:17 +0800

    考试管理模块的试题删除和增加功能

--- vue-element-admin/src/layout/components/Navbar.vue
+++ vue-element-admin/src/layout/components/Navbar.vue
@@ -22,7 +22,7 @@
 
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img :src="userInfo.avatar" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
@@ -70,7 +70,8 @@ export default {
       'sidebar',
       'name',
       'avatar',
-      'device'
+      'device',
+      'userInfo'
     ])
   },
   methods: {

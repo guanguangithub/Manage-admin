da794efefeec5fd3646a70f0b9169fe6eaa010f0
lianjinghong <3405366553@qq.com>
2019-04-25 10:08:40 +0800

    完善待批班级

--- vue-element-admin/src/components/MarkdownEditor/index.vue
+++ vue-element-admin/src/components/MarkdownEditor/index.vue
@@ -33,12 +33,12 @@ export default {
     },
     mode: {
       type: String,
-      default: 'markdown'
+      default: 'wysiwyg'
     },
     height: {
       type: String,
       required: false,
-      default: '500px'
+      default: 'auto'
     },
     language: {
       type: String,
